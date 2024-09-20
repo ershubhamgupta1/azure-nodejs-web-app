b69f43be73c0db43d75c6b9b9227e480352ed1a1
shubham gupta <shubhamgupta@shubhams-MacBook-Air.local>
2024-09-20 11:23:24 +0530

    initial commit for azure nodejs app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve a simple response
+app.get('/', (req, res) => {
+  res.send('Hello, Azure !!!!!');
+});
+
+app.get('/user', (req, res) => {
+  res.send('Hello User 1');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} !!!!!`);
+});
